9002119550fa5b4fefb6e8c4a20b6e68ec281580
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-08-09 11:13:39 -0300

    Agrego Forecast
    Creo otra fetch

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let array = [];
-let url =`https://ws.smn.gob.ar/map_items/weather`
+let arrayForecast = [];
+const url =`https://ws.smn.gob.ar/map_items/weather`
+const urlForecast ="https://ws.smn.gob.ar/forecast/"
+
 let idCity = "609de837818e15902c7513f6"
 const $cityName = document.querySelector("#city-name");
 const $actualTemp = document.querySelector("#actual-temp");
@@ -20,12 +23,13 @@ fetch(url)
             for (let index = 0; index < array.length; index++) {
                 const element = array[index];
                 if (element._id === idCity){
+                    let idForecast = element.lid;
                     console.log(element.lid) // con esto puedo buscar en https://ws.smn.gob.ar/forecast/ el pronostico extendido
                      reach = 1;
                     console.log("Entre")
                     chargeInfo(element);
                     console.log(idCity)
-                    fetchForecast();
+                    fetchForecast(idForecast);
                 }
             }
             if (reach === 0){
@@ -42,19 +46,37 @@ function chargeInfo(element){
                     $cityName.textContent = element.name +" ," + element.province; 
                     $actualTemp.textContent = element.weather.tempDesc
                     $description.textContent = element.weather.description
-                    $wind.textContent =element.weather.wind_speed + " Km/h , Direccion " + element.weather.wing_deg
-                    $humidity.textContent = "La humedad es de "+ element.weather.humidity + " g/m3"
+                    $wind.textContent ="La velocidad del viento es de " +element.weather.wind_speed + " Km/h , Direccion " + element.weather.wing_deg
+                    $humidity.textContent = "La humedad es de "+ element.weather.humidity + " %"
                     $visibility.textContent = "La visibilidad es de "+ element.weather.visibility + " Kilometros."
                     if (element.weather.st != null){
-                        $st.textContent = element.weather.st
+                        $st.textContent = "La sensacion termica es de "+element.weather.st + " C°"
                     }
 }
 
-function fetchForecast(){
-    const urlForecast ="https://ws.smn.gob.ar/forecast/"
+function fetchForecast(idForecast){
     fetch(urlForecast)
     .then(response => response.json())
         .then(data => {
             console.log(data)
+            array = data;
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                if(element.location_id === idForecast){
+                    console.log(element)
+                    console.log("adentro")
+                    reach = 2;
+                    let keys = Object.keys(element.forecast);
+                    console.log(keys)
+                    for (let index = 0; index < keys.length; index++) {
+                        let clave = keys[index];
+                        console.log(keys[clave].date);
+                    }
+                }
+            }
+            if (reach != 2){
+                $actualTemp.textContent = "cargando..."
+                location.reload();
+            }
         })
 }
