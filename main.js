7e6a46e4bf7a3ccea276dc927469b9039be6287e
Alejandro <73189430+Alecapra96@users.noreply.github.com>
2021-08-09 17:30:44 -0300

    Saco pronostico extendido

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let array = [];
 let arrayForecast = [];
 const url =`https://ws.smn.gob.ar/map_items/weather`
-const urlForecast ="https://ws.smn.gob.ar/forecast/"
+// const urlForecast ="https://ws.smn.gob.ar/forecast/"
 
 let idCity = "609de837818e15902c7513f6"
 const $cityName = document.querySelector("#city-name");
@@ -11,8 +11,10 @@ const $wind = document.querySelector("#wind");
 const $st = document.querySelector("#st");
 const $humidity = document.querySelector("#humidity");
 const $visibility = document.querySelector("#visibility");
+const $date = document.querySelector("#date");
+const  $pressure = document.querySelector("#pressure")
 let reach = 0;
-
+let date;
 
 
 fetch(url)
@@ -23,13 +25,13 @@ fetch(url)
             for (let index = 0; index < array.length; index++) {
                 const element = array[index];
                 if (element._id === idCity){
-                    let idForecast = element.lid;
+                    // let idForecast = element.lid;
                     console.log(element.lid) // con esto puedo buscar en https://ws.smn.gob.ar/forecast/ el pronostico extendido
                      reach = 1;
                     console.log("Entre")
                     chargeInfo(element);
                     console.log(idCity)
-                    fetchForecast(idForecast);
+                    // fetchForecast(idForecast);
                 }
             }
             if (reach === 0){
@@ -44,39 +46,51 @@ fetch(url)
 function chargeInfo(element){
     console.log("Felicidades , encontraste la ciudad de idCity")
                     $cityName.textContent = element.name +" ," + element.province; 
+                    $date.textContent = date
                     $actualTemp.textContent = element.weather.tempDesc
                     $description.textContent = element.weather.description
-                    $wind.textContent ="La velocidad del viento es de " +element.weather.wind_speed + " Km/h , Direccion " + element.weather.wing_deg
-                    $humidity.textContent = "La humedad es de "+ element.weather.humidity + " %"
-                    $visibility.textContent = "La visibilidad es de "+ element.weather.visibility + " Kilometros."
-                    if (element.weather.st != null){
-                        $st.textContent = "La sensacion termica es de "+element.weather.st + " C°"
-                    }
-}
+                    $wind.textContent ="Viento  " + element.weather.wing_deg + " a " + element.weather.wind_speed + " Km/h " 
+                    $humidity.textContent = "Humedad:  "+ element.weather.humidity + " %"
+                    $visibility.textContent = "Visibilidad: "+ element.weather.visibility + " km."
+                    $pressure.textContent = "Presion: " + element.weather.pressure+" hPa"
 
-function fetchForecast(idForecast){
-    fetch(urlForecast)
-    .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            array = data;
-            for (let index = 0; index < array.length; index++) {
-                const element = array[index];
-                if(element.location_id === idForecast){
-                    console.log(element)
-                    console.log("adentro")
-                    reach = 2;
-                    let keys = Object.keys(element.forecast);
-                    console.log(keys)
-                    for (let index = 0; index < keys.length; index++) {
-                        let clave = keys[index];
-                        console.log(keys[clave].date);
+                    if (element.weather.st != null){
+                        $st.textContent = "Sensacion termica: "+element.weather.st + " C°"
                     }
-                }
-            }
-            if (reach != 2){
-                $actualTemp.textContent = "cargando..."
-                location.reload();
-            }
-        })
 }
+//___________________________________________________
+// me di cuenta que esta desactualizado hace 2 años est parte de la api
+//___________________________________________________
+// function fetchForecast(idForecast){
+//     fetch(urlForecast)
+//     .then(response => response.json())
+//         .then(data => {
+    
+//             array = data;
+//             for (let index = 0; index < array.length; index++) {
+//                 const element = array[index];
+//                 if(element.location_id === idForecast){
+//                     console.log(element)
+//                     console.log("adentro")
+//                     reach = 2;
+               
+//                     Object.keys(element.forecast).forEach(key => 
+//                         console.log(element.forecast[key])
+//                         )
+//                 }
+//             }
+//             if (reach != 2){
+//                 $actualTemp.textContent = "cargando..."
+//                 location.reload();
+//             }
+//         })
+// }
+function dateTime(){
+var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+var diasSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
+var f=new Date();
+date = diasSemana[f.getDay()] + ", " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear()
+console.log(date);
+return date;
+}
+dateTime();
